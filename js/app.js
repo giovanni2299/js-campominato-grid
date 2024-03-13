54b7f46d83cdaecd75eea754ee7ef9e6bb5d94f2
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-13 15:24:54 +0100

    ADD:grid cell and theyr element

--- js/app.js
+++ js/app.js
@@ -7,4 +7,48 @@
 // Quando l’utente clicca su ogni cella.
 // la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+//collegare il bottone per creare la griglia
+const createGrid = document.querySelector('.btn');
 
+//creo l'evento quando clicco il bottone
+createGrid.addEventListener('click', function() {
+    //creare la griglia 
+    const gridElement = document.querySelector('.grid');
+    //creare le celle all interno della griglia 
+        //do una dimensione alle celle 
+        const dimention = 10;
+        //moltiplico le celle per ottenere una griglia 10 x 10
+        const squereDimention = dimention * dimention;
+
+    //lggo le celle singolarmente per tramite un ciclo for
+
+    for(let i = 0; i < squereDimention; i++){
+
+        //per generare le celle da 1 a 100 altrimenti sarebbe da 0 a 99
+
+        const num = i + 1; 
+
+        //aggiungo le celle al dom all interno di grid
+            //creo un div al interno del dom 
+
+        const cellElement = document.createElement('div');
+            //gli assegno la classe cell 
+
+        cellElement.className = 'cell';
+
+            //stampo in numeri all interno delle nostre celle per averle numerate da 1 a 100
+
+        cellElement.innerHTML = num;
+
+            //"appendo" le mie celle al interno del grid
+
+        gridElement.appendChild(cellElement);
+
+        //ad ogni re-click le celle perdono il colore dark green
+        cellElement.addEventListener('click', function(){
+            cellElement.classList.toggle('bg-dark-green');
+            console.log(num);
+        })
+    }
+
+})
